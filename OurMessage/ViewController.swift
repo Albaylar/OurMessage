18189b7d59a2b9ce5f8d63a29d71dd93def438d9
Albaylar <furkandenizalbaylar@gmail.com>
2022-07-06 14:53:15 +0300

    Getting started

---
+++ OurMessage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OurMessage
+//
+//  Created by Furkan Deniz Albaylar on 6.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
